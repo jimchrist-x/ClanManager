f5ad3ac7f83846edbfebd9466c0335b4b22b3569
jimchrist-x <jimchrist2@gmail.com>
2025-09-28 23:41:32 +0300

    Expand member details output in example print statement

--- src/main/java/com/jimchrist/clanmanager/ClanManager.java
+++ src/main/java/com/jimchrist/clanmanager/ClanManager.java
@@ -13,7 +13,16 @@ public class ClanManager {
                     case "claninfo":
                         ClanInfo clanInfo = new ClanInfo(clashRoyale);
                         for (int i=0;i<clanInfo.getMembers();i++) {
-                            System.out.println(clanInfo.getMember(i).getName());
+                            System.out.printf("Players tag is %s his/her nickname is %s. He/her is a %s in the clan.\n" +
+                                    "He/her was last seen at %s. His/her stats are:\nExpLevel: %d\nTrophies: %d\nArena: %s\n" +
+                                    "His/her clan profile includes:\nClanRank: %d\nPrevClanRank: %d\nDonations: %d\nDonations received: %d\n" +
+                                            "ClanChestPoints: %d", clanInfo.getMember(i).getTag(), clanInfo.getMember(i).getName(), clanInfo.getMember(i).getRole(),
+                                    clanInfo.getMember(i).getLastSeen(), clanInfo.getMember(i).getExpLevel(), clanInfo.getMember(i).getTrophies(),
+                                    clanInfo.getMember(i).getArena().getName(), clanInfo.getMember(i).getClanRank(), clanInfo.getMember(i).getPrevClanRank(),
+                                    clanInfo.getMember(i).getDonations(), clanInfo.getMember(i).getDonationsReceived(),
+                                    clanInfo.getMember(i).getClanChestPoints());
+                            System.out.println();
+                            System.out.println("===================================================================================");
                         }
                         break;
                     default:
